379c65e22f8d8b126d4649b9dece539daa849b67
Josh <josh@upstash.com>
2025-07-23 16:33:02 +0200

    chore: add login error logging

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,12 +3,15 @@
 import { authClient } from '@/lib/auth-client'
 import { useEffect } from 'react'
 import toast from 'react-hot-toast'
+import posthog from 'posthog-js'
 
 const LoginPage = () => {
   const handleAccess = async () => {
     const { data, error } = await authClient.signIn.social({ provider: 'google' })
 
     if (error) {
+      posthog.captureException(error)
+
       toast.error(
         error.message ?? 'An error occurred, please DM @joshtriedcoding on twitter!',
       )
